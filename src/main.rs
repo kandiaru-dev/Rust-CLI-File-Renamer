0e67d1cf06e917a6474e566b104f86ef16bc6475
Dipesh Gavit <dipeshgavit333@gmail.com>
2025-09-15 02:37:06 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+fn main() {
+
+    let paths = fs::read_dir("./public").unwrap();
+
+    for path in paths {
+        let path = path.unwrap().path();
+        let old_name = path.to_string_lossy();
+        let new_name = old_name.to_lowercase();
+
+        fs::rename(&*old_name, &new_name).unwrap();
+
+        println!("renamed file: {} -> {}", old_name, new_name);
+    }
+}
